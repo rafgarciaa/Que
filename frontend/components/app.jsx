c67778285cbed0dc5663b5cb9c3a09776bcbc593
Rafael Garcia <rafgarciaa@gmail.com>
2018-09-17 08:56:03 -0400

    Add protected route for /answer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,13 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import SessionForm from './session/session_form';
 import Homepage from './homepage';
-
+import NavbarContainer from './navbar/navbar_container';
 
 const App = () => (
   <div>
     <AuthRoute path="/" component={SessionForm} />
 
     <Switch>
+      <ProtectedRoute path='/answer' component={NavbarContainer} />
       <ProtectedRoute exact path='/index' component={Homepage} />
     </Switch>
   </div>
