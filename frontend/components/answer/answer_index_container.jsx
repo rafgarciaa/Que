07600b6a578236763f8a85710cc542dfd950b4c0
Rafael Garcia <rafgarciaa@gmail.com>
2018-09-20 09:56:49 -0400

    Add answer index container

---
+++ frontend/components/answer/answer_index_container.jsx
@@ -0,0 +1,24 @@
+import AnswerIndex from './answer_index';
+import { connect } from 'react-redux';
+import { fetchQuestions } from '../../actions/question_actions';
+import { fetchAnswers } from '../../actions/answer_actions';
+import { selectAllQuestions, selectAllAnswers } from '../../reducers/selectors';
+
+const msp = state => {
+  return {
+    users: state.entities.users,
+    topics: state.entities.topics,
+    currentUser: state.entities.users[state.session.id],
+    questions: selectAllQuestions(state),
+    answers: selectAllAnswers(state),
+  };
+};
+
+const mdp = dispatch => {
+  return {
+    fetchQuestions: () => dispatch(fetchQuestions()),
+    fetchAnswers: () => dispatch(fetchAnswers()),
+  };
+};
+
+export default connect(msp, mdp)(AnswerIndex);
